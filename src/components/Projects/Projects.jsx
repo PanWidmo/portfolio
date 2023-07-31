9218274f4e34ab4f363043276b3901202c50e4b3
PanWidmo <tomasz.baranww@gmail.com>
2023-07-31 15:54:31 +0200

    Change link address

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,7 +6,7 @@ export const Projects = () => {
         <section id="projects" className={styles.projects}>
             <h2>Projects</h2>
             <Slider />
-            <a href="https://github.com/PanWidmo" target="_blank" rel="noreferrer" className={styles.projects__button}>
+            <a href="https://github.com/tomaszbaranww" target="_blank" rel="noreferrer" className={styles.projects__button}>
                 See more projects
             </a>
         </section>
